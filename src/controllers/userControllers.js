008d762222a8c9e1d4c8faa9ccd0cf9855dfc589
laptop <chemseddine.hala@gmail.com>
2026-03-30 14:39:22 +0100

    some improvements

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,13 +3,13 @@ const AppError = require("../utils/AppError");
 const asyncHandler = require("../utils/asyncHandler");
 // get all users
 const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
+  const users = await User.find().select("-password");
   res.status(200).json(users);
 });
 
 // get one user by id
 const getUserById = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).select("-password");
   if (!user) throw new AppError("User not found", 404);
   res.status(200).json(user);
 });
